c071dab39609a3ce832708cff3c96c6dd2d03b67
vahid Afshari <vahidinline@gmail.com>
2022-10-28 15:46:03 +0300

    add flags

--- src/App.js
+++ src/App.js
@@ -7,25 +7,37 @@ import {
   ButtonGroup,
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import getUnicodeFlagIcon from 'country-flag-icons/unicode';
 
 function App() {
   return (
     <Container>
       <Row>
-        <div className="fs-3 text-justify">
-          We have formed this human chain to support human rights against
-          dictatorships in Iran and Russia. If you believe in human rights,
-          stand by us.
+        <div className="fs-5 text-justify">
+          We now have common ENEMIES! While Ukrainian people are fighting
+          against Putin, In Iran, people are fighting for freedom, against the
+          dictators who are killing, raping and suppressing innocents meanwhile
+          supporting Putin with war drones to attack Ukraine. In solidarity with
+          the people of Ukraine and Iran, we are going to have a human chain
+          from Vabaduse valjak to the Embassy of Russia in Tallinn. Permission
+          for this event has been granted by Police. Feel free to take your
+          banners, photos and flags with you.
         </div>
       </Row>
       <div className="d-grid gap-2">
-        <Button variant="success" size="lg">
+        <Button
+          variant="secondary"
+          size="lg"
+          onClick={() => window.open('https://streamable.com/5xri27')}>
+          >{getUnicodeFlagIcon('EE')}
           Eesti keel
         </Button>
-        <Button variant="light" size="lg">
+        <Button variant="secondary" size="lg">
+          {getUnicodeFlagIcon('UA')}
           украї́нська мо́ва
         </Button>
-        <Button variant="danger" size="lg">
+        <Button variant="secondary" size="lg">
+          {getUnicodeFlagIcon('RU')}
           русский язык
         </Button>
       </div>
